ed80f781ea638ce0b21ce731dc22a5903c18c4da
GabrielDiniz <aluisiogabrieldiniz@gmail.com>
2026-05-17 18:46:47 -0300

    terceiro deploy

--- back/app.py
+++ back/app.py
@@ -38,7 +38,7 @@ def cozinha_page():
 dbconfig = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASS"),
+    "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME")
 }
 
